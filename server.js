c7eb9abb4398ebf88230fecd3caca0b1f08ec1b0
mohammad-zytoon <mohamadzaiton@gmail.com>
2021-05-24 22:27:33 +0300

    achived

--- server.js
+++ server.js
@@ -58,38 +58,5 @@ server.get('/weather', (req, res) => {
 
 
 
-// class ForCast {
-//     constructor(date, description) {
-//         this.date = date;
-//         this.description = description;
-//     }
-// }
-// server.get('/weather', (req, res) => {
-//     let city = req.query.searchQuery;
-//     let e = true;
-//     let found = weatherData.find((element) => {
-//         if (city.toLowerCase() == element.city_name.toLowerCase()) {
-//             e = false;
-//             return element;
-//         }
-//     })
-//     if (e == false) {
-//         // date = found.data[0].valid_date
-//         // `low of ${found.data[0].min_temp}, hight of${found.data[0].max_temp} with ${found.data[0].weather.description}`
-//         let forcastArr = [];
-//         let date;
-//         let description;
-//         let forcastData;
-//         for(let i=0;i<found.data.length;i++){
-//             date = found.data[0].valid_date;
-//             description = `low of ${found.data[i].min_temp}, hight of${found.data[i].max_temp} with ${found.data[i].weather.description}`;
-//             forcastData = new ForCast(date,description);
-//             forcastArr.push(forcastData);
-//         }
-        
-//         res.send(forcastArr);
-//     } else {
-//         res.send('error');
-//     }
-// })
+
 
